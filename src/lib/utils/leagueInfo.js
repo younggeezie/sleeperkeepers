0793e979e00e500b1fc1cb8e0386af6cbd029170
younggeezie <63011162+younggeezie@users.noreply.github.com>
2023-09-09 10:02:50 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -115,8 +115,8 @@ export const managers = [
  },
  {
    "roster": 5,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-   "name": "Your Name",
-   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+   "name": "Jeff Christie",
+   // "tookOver": , // (optional) used if a manager took over a team, delete this line or change to null otherwise
    "location": "Brooklyn", // (optional)
    "bio": "Lorem ipsum...",
    "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
